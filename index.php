df17234f583259994ff2649de1aabb225515aa06
Vilhelm Prytz <vpytkanal.sverige@gmail.com>
2017-03-12 19:16:53 +0100

    Inital code for site

---
+++ index.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>MrKaKisen</title>
+
+	<!-- Website made by Vilhelm Prytz.
+			(c) Copyright Vilhelm Prytz 2017
+			Version: 1.3
+	-->
+
+	<link rel="icon" href="./assets/img/round-32x32.png" sizes="32x32" />
+	<link rel="icon" href="./assets/img/round-192x192.png" sizes="192x192" />
+	<link rel="apple-touch-icon-precomposed" href="./assets/img/round-180x180.pn" />
+
+	<meta charset="UTF-8">
+	<meta name="description" content="Personal page for Vilhelm Prytz (MrKaKisen)">
+	<meta name="keywords" content="MrKaKisen,MrKaKisen.se,Vilhelm Prytz,Prytz Vilhelm">
+
+	<link href="./assets/css/main.css" rel="stylesheet" type="text/css" />
+	<link rel="stylesheet" media="screen and (min-width: 1px) and (max-width: 1120px)" href="./assets/css/mobile.css" />
+	<link href="./assets/css/goodies.css" rel="stylesheet" type="text/css" />
+	<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+</head>
+<body backgroun="#FFFFF" class="body">
+	<div class="bigPictueBox">
+		<img src="./assets/img/mountain3-small.png" class="bigPicture">
+	</div>
+	<div class="welcome-about" id="introduction">
+		<font face="Lato">
+			<h1>MrKaKisen</h1>
+			<p>Welcome to MrKaKisen!
+			<br>Home of Vilhelm Prytz, <a href="https://dns.mrkakisen.net/" class="links">MrKaKisen DNS</a> and much more!</p>
+			<img src="./assets/img/round-180x180.png" height="150" width="150" class="fade">
+		</font>
+	</div>
+
+	<div class="welcome-about" id="projects">
+		<font face="Lato">
+			<h1>Projects</h1>
+			<p>Read more about some of the projects I'm working
+			<br>on over at <a href="https://projects.mrkakisen.net" class="links">projects.mrkakisen.net</a>.</p>
+		</font>
+	</div>
+
+	<div class="welcome-about floatRight" id="about-me">
+		<font face="Lato">
+			<h1>About Me</h1>
+			<p>Hi! My name is Vilhelm Prytz, also known as 'MrKaKisen'.
+			<br>I'm a Swedish system administrator and developer.</p>
+			<p>I mainly do System Administration tasks (Linux), but I've also
+			<br>developed in Python, Flask, HTML and more (currently learning C++).</p>
+			<p>My favorite Linux distribution is CentOS, however
+			<br>I'm also very familiar with Debian and Ubuntu (aswell as other distros).
+		</font>
+	</div>
+
+	<div class="opactiyHalf">
+		<tr><th colspan="5"><hr></th></tr>
+	</div>
+
+	<center>
+		<font face="Lato">
+			<h1 class="invisible">Services</h1>
+		</font>
+	</center>
+
+	<div class="services" id="dns-service">
+		<font face="Lato">
+			<h1>MrKaKisen DNS</h1>
+			<p>Built across the world, MrKaKisen DNS is an advanced DNS service
+			<br>for both individuals and companies.
+			<br>With full IPv6 support, and servers with many different providers,
+			<br>we belive we have one of the most reliable DNS services out there.
+			<br><a href="https://dns.mrkakisen.net/" class="links">Read more here.</a></p>
+		</font>
+	</div>
+
+	<div class="services" id="system-administration">
+		<font face="Lato">
+			<h1>System Administration</h1>
+			<p>All systems and servers need system administration to run safely
+			<br>and run at the best performance possible, without hiccups.
+			<br>With System Administration, I will install software, fix bugs
+			<br>and help configuring applications to your server.
+			<br>Whatever it is, I have a lot of experience with Linux.
+			<br><a href="contact.php" class="links">Contact me for a quote.</a></p>
+		</font>
+	</div>
+
+	<div class="services" id="managed-web">
+		<font face="Lato">
+			<h1>Managed Web Hosting</h1>
+			<p>With managed web hosting, you will never have to
+			<br>worry about maintenance and service disruptions.
+			<br>MrKaKisen DNS included. <a href="contact.php" class="links">Contact me for a quote.</a></p>
+
+		</font>
+	</div>
+
+	<div class="opactiyHalf">
+		<tr><th colspan="5"><hr></th></tr>
+	</div>
+
+	<div class="footer opactiyHalf" id="footer">
+		<font face="Lato" color="black">
+			<p>© Vilhelm Prytz <?php echo date("Y"); ?>
+			<br>
+			<br><a href="https://twitter.com/VilhelmPrytz" class="links2" id="twitter">Twitter</a>
+			<br><a href="https://github.com/MrKaKisen" class="links2" id="github">GitHub</a>
+			<br>Discord - MrKaKisen#8523
+			<br><a href="mailto:contact@mrkakisen.net" class="links2" id="email">contact(at)mrkakisen.net</a></p>
+		</font>
+	</div>
+
+	<div class="footer opactiyHalf" id="footerRight">
+		<font face="Lato" color="black">
+			<p>Images by me under <a href="https://plentifulphotos.se/" class="links2">PlentifulPhotos</a>.
+			<br><a href="https://status.mrkakisen.net" class="links2">Server Status Page</a></p>
+		</font>
+	</div>
+</body>
+</html>
