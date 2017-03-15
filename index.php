ae60ae663f16e14bf5087ce529060f0fc3d2166c
JonasHTV7 <jonas@htv7.net>
2017-03-15 07:16:16 +0100

    Update index.php
    Made the image position fixed and done width with percentages.

--- index.php
+++ index.php
@@ -16,92 +16,93 @@
 	<link href="./assets/css/goodies.css" rel="stylesheet" type="text/css">
 	<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
 </head>
-<body backgroun="#FFFFF" class="body">
-	<div class="bigPictueBox">
-		<img src="./assets/img/mountain3-small.png" class="bigPicture">
+<body>
+	<div class="bigImgBox">
+		<img src="./assets/img/mountain3-small.png" class="bigImg">
+		<div class="bigImgPusher"></div>
 	</div>
-	<div class="welcome-about" id="introduction">
-		<font face="Lato">
-			<h1>MrKaKisen</h1>
-			<p>Welcome to MrKaKisen!
-			<br>Home of Vilhelm Prytz, <a href="https://dns.mrkakisen.net/" class="links">MrKaKisen DNS</a> and much more!</p>
-			<img src="./assets/img/round-180x180.png" height="150" width="150" class="fade">
-		</font>
-	</div>
-	<div class="welcome-about" id="projects">
-		<font face="Lato">
-			<h1>Projects</h1>
-			<p>Read more about some of the projects I'm working
-			<br>on over at <a href="https://projects.mrkakisen.net" class="links">projects.mrkakisen.net</a>.</p>
-		</font>
-	</div>
-	<div class="welcome-about floatRight" id="about-me">
-		<font face="Lato">
-			<h1>About Me</h1>
-			<p>Hi! My name is Vilhelm Prytz, also known as 'MrKaKisen'.
-			<br>I'm a Swedish system administrator and developer.</p>
-			<p>I mainly do System Administration tasks (Linux), but I've also
-			<br>developed in Python, Flask, HTML and more (currently learning C++).</p>
-			<p>My favorite Linux distribution is CentOS, however
-			<br>I'm also very familiar with Debian and Ubuntu (aswell as other distros).
-		</font>
-	</div>
-	<div class="opactiyHalf">
-		<tr><th colspan="5"><hr></th></tr>
-	</div>
-	<center>
-		<font face="Lato">
+	<div class="content">
+		<section class="content-section" id="content-welcome">
+			<div class="welcome-about" id="introduction">
+				<font face="Lato">
+					<h1>MrKaKisen</h1>
+					<p>Welcome to MrKaKisen!
+					<br>Home of Vilhelm Prytz, <a href="https://dns.mrkakisen.net/" class="links">MrKaKisen DNS</a> and much more!</p>
+					<img src="./assets/img/round-180x180.png" height="150px" width="150px" class="fade">
+				</font>
+			</div>
+			<div class="welcome-about" id="projects">
+				<font face="Lato">
+					<h1>Projects</h1>
+					<p>Read more about some of the projects I'm working
+					<br>on over at <a href="https://projects.mrkakisen.net" class="links">projects.mrkakisen.net</a>.</p>
+				</font>
+			</div>
+			<div class="welcome-about" id="about-me">
+				<font face="Lato">
+					<h1>About Me</h1>
+					<p>Hi! My name is Vilhelm Prytz, also known as 'MrKaKisen'.
+					<br>I'm a Swedish system administrator and developer.</p>
+					<p>I mainly do System Administration tasks (Linux), but I've also
+					<br>developed in Python, Flask, HTML and more (currently learning C++).</p>
+					<p>My favorite Linux distribution is CentOS, however
+					<br>I'm also very familiar with Debian and Ubuntu (aswell as other distros).
+				</font>
+			</div>
+		</section>
+		<div class="section-line"></div>
+		<section class="content-section" id="content-services">
 			<h1 class="invisible">Services</h1>
-		</font>
-	</center>
-	<div class="services" id="dns-service">
-		<font face="Lato">
-			<h1>MrKaKisen DNS</h1>
-			<p>Built across the world, MrKaKisen DNS is an advanced DNS service
-			<br>for both individuals and companies.
-			<br>With full IPv6 support, and servers with many different providers,
-			<br>we belive we have one of the most reliable DNS services out there.
-			<br><a href="https://dns.mrkakisen.net/" class="links">Read more here.</a></p>
-		</font>
-	</div>
-	<div class="services" id="system-administration">
-		<font face="Lato">
-			<h1>System Administration</h1>
-			<p>All systems and servers need system administration to run safely
-			<br>and run at the best performance possible, without hiccups.
-			<br>With System Administration, I will install software, fix bugs
-			<br>and help configuring applications to your server.
-			<br>Whatever it is, I have a lot of experience with Linux.
-			<br><a href="contact.php" class="links">Contact me for a quote.</a></p>
-		</font>
-	</div>
-	<div class="services" id="managed-web">
-		<font face="Lato">
-			<h1>Managed Web Hosting</h1>
-			<p>With managed web hosting, you will never have to
-			<br>worry about maintenance and service disruptions.
-			<br>MrKaKisen DNS included. <a href="contact.php" class="links">Contact me for a quote.</a></p>
+			<div class="services" id="dns-service">
+				<font face="Lato">
+					<h1>MrKaKisen DNS</h1>
+					<p>Built across the world, MrKaKisen DNS is an advanced DNS service
+					<br>for both individuals and companies.
+					<br>With full IPv6 support, and servers with many different providers,
+					<br>we belive we have one of the most reliable DNS services out there.
+					<br><a href="https://dns.mrkakisen.net/" class="links">Read more here.</a></p>
+				</font>
+			</div>
+			<div class="services" id="system-administration">
+				<font face="Lato">
+					<h1>System Administration</h1>
+					<p>All systems and servers need system administration to run safely
+					<br>and run at the best performance possible, without hiccups.
+					<br>With System Administration, I will install software, fix bugs
+					<br>and help configuring applications to your server.
+					<br>Whatever it is, I have a lot of experience with Linux.
+					<br><a href="contact.php" class="links">Contact me for a quote.</a></p>
+				</font>
+			</div>
+			<div class="services" id="managed-web">
+				<font face="Lato">
+					<h1>Managed Web Hosting</h1>
+					<p>With managed web hosting, you will never have to
+					<br>worry about maintenance and service disruptions.
+					<br>MrKaKisen DNS included. <a href="contact.php" class="links">Contact me for a quote.</a></p>
 
-		</font>
-	</div>
-	<div class="opactiyHalf">
-		<tr><th colspan="5"><hr></th></tr>
-	</div>
-	<div class="footer opactiyHalf" id="footer">
-		<font face="Lato" color="black">
-			<p>© Vilhelm Prytz <?php echo date("Y"); ?>
-			<br>
-			<br><a href="https://twitter.com/VilhelmPrytz" class="links2" id="twitter">Twitter</a>
-			<br><a href="https://github.com/MrKaKisen" class="links2" id="github">GitHub</a>
-			<br>Discord - MrKaKisen#8523
-			<br><a href="mailto:contact@mrkakisen.net" class="links2" id="email">contact(at)mrkakisen.net</a></p>
-		</font>
-	</div>
-	<div class="footer opactiyHalf" id="footerRight">
-		<font face="Lato" color="black">
-			<p>Images by me under <a href="https://plentifulphotos.se/" class="links2">PlentifulPhotos</a>.
-			<br><a href="https://status.mrkakisen.net" class="links2">Server Status Page</a></p>
-		</font>
+				</font>
+			</div>
+		</section>
+		<div class="section-line"></div>
+		<section class="content-section" id="content-footer">
+			<div class="footer opactiyHalf" id="footer">
+				<font face="Lato" color="black">
+					<p>© Vilhelm Prytz <?php echo date("Y"); ?>
+					<br>
+					<br><a href="https://twitter.com/VilhelmPrytz" class="links2" id="twitter">Twitter</a>
+					<br><a href="https://github.com/MrKaKisen" class="links2" id="github">GitHub</a>
+					<br>Discord - MrKaKisen#8523
+					<br><a href="mailto:contact@mrkakisen.net" class="links2" id="email">contact(at)mrkakisen.net</a></p>
+				</font>
+			</div>
+			<div class="footer opactiyHalf" id="footerRight">
+				<font face="Lato" color="black">
+					<p>Images by me under <a href="https://plentifulphotos.se/" class="links2">PlentifulPhotos</a>.
+					<br><a href="https://status.mrkakisen.net" class="links2">Server Status Page</a></p>
+				</font>
+			</div>
+		</section>
 	</div>
 </body>
 </html>
