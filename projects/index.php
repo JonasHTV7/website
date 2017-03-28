56fb98b7b96e2b47e76451e5cf4ce9f18ca99060
JonasHTV7 <jonas@htv7.net>
2017-03-28 17:59:33 +0200

    Update index.php

--- projects/index.php
+++ projects/index.php
@@ -1,106 +1,69 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>MrKaKisen</title>
+	<title>MrKaKisen - Projects</title>
 	<!-- Website made by Vilhelm Prytz and "JonasHTV7".
 			(c) Copyright Vilhelm Prytz 2017
-			Version: 1.3 -->
-	<link rel="icon" href="./assets/img/round-32x32.png" sizes="32x32">
-	<link rel="icon" href="./assets/img/round-192x192.png" sizes="192x192">
-	<link rel="apple-touch-icon-precomposed" href="./assets/img/round-180x180.png">
+      Version: 1.4
+	-->
+	<link rel="icon" href="https://mrkakisen.se/assets/img/round-32x32.png" sizes="32x32" />
+	<link rel="icon" href="https://mrkakisen.se/assets/img/round-192x192.png" sizes="192x192" />
+	<link rel="apple-touch-icon-precomposed" href="https://mrkakisen.se/assets/img/round-180x180.pn" />
 	<meta charset="UTF-8">
 	<meta name="description" content="Personal page for Vilhelm Prytz (MrKaKisen)">
 	<meta name="keywords" content="MrKaKisen,MrKaKisen.se,Vilhelm Prytz,Prytz Vilhelm">
-	<link href="./assets/css/main.css" rel="stylesheet" type="text/css">
-	<link rel="stylesheet" media="screen and (min-width: 1px) and (max-width: 1120px)" href="./assets/css/mobile.css">
-	<link href="./assets/css/goodies.css" rel="stylesheet" type="text/css">
+	<link href="../assets/css/main.css" rel="stylesheet" type="text/css" />
+	<link href="../assets/css/goodies.css" rel="stylesheet" type="text/css" />
 	<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
 </head>
 <body>
 	<div class="bigImgBox">
-		<img src="./assets/img/mountain3-small.png" class="bigImg">
+		<img src="https://mrkakisen.se/assets/img/mountain3-small.png" class="bigImg">
 		<div class="bigImgPusher"></div>
 	</div>
 	<div class="content">
 		<section class="content-section" id="content-welcome">
-			<div class="welcome-about" id="introduction">
-				<font face="Lato">
-					<h1>MrKaKisen</h1>
-					<p>Welcome to MrKaKisen!
-					<br>Home of Vilhelm Prytz, <a href="https://dns.mrkakisen.net/" class="links">MrKaKisen DNS</a> and much more!</p>
-					<img src="./assets/img/round-180x180.png" height="150px" width="150px" class="fade">
-				</font>
-			</div>
-			<div class="welcome-about" id="projects">
-				<font face="Lato">
-					<h1>Projects</h1>
-					<p>Read more about some of the projects I'm working
-					<br>on over at <a href="https://projects.mrkakisen.net" class="links">projects.mrkakisen.net</a>.</p>
-				</font>
-			</div>
-			<div class="welcome-about" id="about-me">
-				<font face="Lato">
-					<h1>About Me</h1>
-					<p>Hi! My name is Vilhelm Prytz, also known as 'MrKaKisen'.
-					<br>I'm a Swedish system administrator and developer.</p>
-					<p>I mainly do System Administration tasks (Linux), but I've also
-					<br>developed in Python, Flask, HTML and more (currently learning C++).</p>
-					<p>My favorite Linux distribution is CentOS, however
-					<br>I'm also very familiar with Debian and Ubuntu (aswell as other distros).
-				</font>
+			<div class="project-info" id="introduction">
+				<h1>Projects</h1>
+				<p>Here are a list of some of the projects that I've been working on / currently working on.</p>
+    			<p><a href="https://mrkakisen.se/" class="link">Return to homepage.</a></p>
 			</div>
 		</section>
-		<div class="section-line"></div>
-		<section class="content-section" id="content-services">
-			<h1 class="invisible">Services</h1>
-			<div class="services" id="dns-service">
-				<font face="Lato">
-					<h1>MrKaKisen DNS</h1>
-					<p>Built across the world, MrKaKisen DNS is an advanced DNS service
-					<br>for both individuals and companies.
-					<br>With full IPv6 support, and servers with many different providers,
-					<br>we belive we have one of the most reliable DNS services out there.
-					<br><a href="https://dns.mrkakisen.net/" class="links">Read more here.</a></p>
-				</font>
+		<div class="section-line opactiyHalf"></div>
+		<section class="content-section" id="content-projects">
+			<div class="projects" id="SkandiServers">
+				<h1>SkandiServers</h1>
+				<p>I do different System Administration and development tasks for SkandiServers.
+				<br><a target="_blank" href="https://skandiservers.net/" class="link">Visit website.</a></p>
 			</div>
-			<div class="services" id="system-administration">
-				<font face="Lato">
-					<h1>System Administration</h1>
-					<p>All systems and servers need system administration to run safely
-					<br>and run at the best performance possible, without hiccups.
-					<br>With System Administration, I will install software, fix bugs
-					<br>and help configuring applications to your server.
-					<br>Whatever it is, I have a lot of experience with Linux.
-					<br><a href="contact.php" class="links">Contact me for a quote.</a></p>
-				</font>
+			<div class="projects" id="NodeShooter">
+				<h1>NodeShooter</h1>
+				<p>Not-yet-released game made in python and pygame.
+				<br><a target="_blank" href="https://nodeshooter.com/" class="link">Visit website.</a></p>
 			</div>
-			<div class="services" id="managed-web">
-				<font face="Lato">
-					<h1>Managed Web Hosting</h1>
-					<p>With managed web hosting, you will never have to
-					<br>worry about maintenance and service disruptions.
-					<br>MrKaKisen DNS included. <a href="contact.php" class="links">Contact me for a quote.</a></p>
-
-				</font>
+			<div class="projects" id="website">
+				<h1>This website</h1>
+				<p>You can view the source code of this website on GitHub.
+				<br><a target="_blank" href="https://github.com/MrKaKisen/website" class="link">Visit GitHub.</a></p>
+			</div>
+			<div class="projects" id="website">
+				<h1>And a lot more..</h1>
+				<p>All repositories are available on GitHub.
+				<br><a target="_blank" href="https://github.com/MrKaKisen" class="link">Visit GitHub.</a></p>
 			</div>
 		</section>
-		<div class="section-line"></div>
+		<div class="section-line opactiyHalf"></div>
 		<section class="content-section" id="content-footer">
 			<div class="footer opactiyHalf" id="footer">
-				<font face="Lato" color="black">
-					<p>© Vilhelm Prytz <?php echo date("Y"); ?>
-					<br>
-					<br><a href="https://twitter.com/VilhelmPrytz" class="links2" id="twitter">Twitter</a>
-					<br><a href="https://github.com/MrKaKisen" class="links2" id="github">GitHub</a>
-					<br>Discord - MrKaKisen#8523
-					<br><a href="mailto:contact@mrkakisen.net" class="links2" id="email">contact(at)mrkakisen.net</a></p>
-				</font>
+				<p>© Vilhelm Prytz <?php echo date("Y"); ?><br>
+				<br><a href="https://twitter.com/VilhelmPrytz" class="link2" id="twitter">Twitter</a>
+				<br><a href="https://github.com/MrKaKisen" class="link2" id="github">GitHub</a>
+				<br>Discord - MrKaKisen#8523
+				<br><a href="mailto:contact@mrkakisen.net" class="link2" id="email">contact(at)mrkakisen.net</a></p>
 			</div>
 			<div class="footer opactiyHalf" id="footerRight">
-				<font face="Lato" color="black">
-					<p>Images by me under <a href="https://plentifulphotos.se/" class="links2">PlentifulPhotos</a>.
-					<br><a href="https://status.mrkakisen.net" class="links2">Server Status Page</a></p>
-				</font>
+				<p>Images by me under <a href="https://plentifulphotos.se/" class="link2">PlentifulPhotos</a>.
+				<br><a href="https://status.mrkakisen.net" class="link2">Server Status Page</a></p>
 			</div>
 		</section>
 	</div>
